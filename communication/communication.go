84a1ffd137c45a2a68d53d4786e4041eecb48f0b
Hillestad23 <martinhillestad04@gmail.com>
2025-03-04 20:15:27 +0100

    lagt til noen ting i configuration og litt i communication

--- communication/communication.go
+++ communication/communication.go
@@ -1,16 +1,57 @@
 package communication
 
-func CommunicationHandler(
-	// channels som må tas inn
-	elevatorID string,
-) {
+import (
+	"fmt"
+	"TTK4145-Heislab/Network-go/network/peers"
+	"TTK4145-Heislab/single_elevator"
+	"TTK4145-Heislab/configuration"
+)
+
+func CommunicationHandler(chans configuration.CommunicationChannels) {
+
+	//initialisering
+	numPeers := 0
+
 
 	for {
-	OuterLoop: //break ut av hele for-loopen
+	
 		select {
+		//case_ 5: Oppdateringer for den lokale heisen
+		case newLocalElevator := <-newLocalElevatorChannel: 
+			localWorldView.ElevatorList[elevatorID] = newLocalElevator
+			cabReq := GetCabRequests(newLocalElevator)
 
-		case newLocalElevator := <-newLocalElevatorChannel: //heis forsvunnet og kommet tilbake på nettverket
-		case peers := <-peerUpdateChannel: //oppdatere på hvilke heiser som er aktive
+			// Lagre bestillingene til en JSON-fil som en backup. mby
+			//saveJSON.SaveCabButtonToFile(cabReq, "cabOrders.json")
+		
+
+		//Case 6:
+		//oppdatere på hvilke heiser som er aktive ( når heiser kommer på og forsvinner fra nettverket)
+		case peers := <-peerUpdateChannel: 
+			
+			fmt.Printf("Peer update:\n")
+			fmt.Printf("  Peers:    %q\n", peers.Peers)
+			fmt.Printf("  New:      %q\n", peers.New)
+			fmt.Printf("  Lost:     %q\n", peers.Lost)
+			
+			numPeers = len(peers.Peers)
+
+			if len(peers.Lost) > 0 {
+				if localWorldView.ElevatorList[peers.Lost[0]].Unavailable {
+					AssignOrder(*localWorldView, ch_assignedRequests)
+					peerTXEnableChannel <- true
+				} else {
+					for i, ack := range localWorldView.AckList {
+						for _, lostPeer := range peers.Lost {
+							delete(localWorldView.ElevatorList, lostPeer)
+							if ack == lostPeer {
+								localWorldView.AckList = append(localWorldView.AckList[:i], localWorldView.AckList[i+1:]...)
+							}
+						}
+					}
+					AssignOrder(*localWorldView, assignedRequestsChannel)
+				}
+		}
 		}
 	}
 }
--- configuration/configuration.go
+++ configuration/configuration.go
@@ -2,6 +2,8 @@ package configuration
 
 import (
 	"time"
+	
+
 )
 
 const (
@@ -26,3 +28,47 @@ const (
 )
 
 //legge typen i configuration. Lage kanalene de skal sendes på i main.g. structuren på hva som blir sendt på kanalen
+
+//hva skla detligge i const, struct?
+
+/*
+type CommunicationChannels struct{
+	
+	elevatorID string
+	peerUpdateChannel <- chan peers.PeerUpdate
+	NewlocalElevatorChannel <- chan single_elevator.State
+	peerTXEnableChannel chan<- bool
+	
+}
+	//assignedRequestsChannel chan<- map[string][][2]bool, 
+	// trenger worldwiew også
+
+/*
+		ElevatorID               string                         // Identifikator for heisen
+		PeerUpdateChannel        <-chan peers.PeerUpdate         // Mottar oppdateringer om peers
+		NewLocalElevatorChannel  <-chan single_elevator.State    // Mottar oppdateringer for den lokale heisen
+		PeerTXEnableChannel      chan<- bool                    // Sender signal for å aktivere sending til peers
+		AssignedRequestsChannel  chan<- map[string][][2]bool      // Sender tilordnede hallordrer (hvis du trenger dette)
+		// Eventuelt flere kanaler:
+		MessageTxChannel         chan<- WorldView               // Sender ut WorldView
+		MessageRxChannel         <-chan WorldView               // Mottar WorldView fra andre heiser
+		OrderChannel             <-chan elevio.ButtonEvent      // Mottar knappetrykk (ordrer)
+		CompleteOrderChannel     <-chan fsm.DoneOrder           // Mottar signal om fullførte ordrer
+		ConfirmedOrderChannel    chan WorldView                 // Sender bekreftede ordre (om nødvendig)
+		MergeFSMChannel          chan<- elevio.ButtonEvent      // Sender ordrer direkte til FSM hvis heisen er alene
+*/
+
+type CommunicationChannels struct {
+    ElevatorID               string
+    PeerUpdateChannel        <-chan peers.PeerUpdate
+    NewLocalElevatorChannel  <-chan single_elevator.State
+    PeerTXEnableChannel      chan<- bool
+    AssignedRequestsChannel  chan<- map[string][][2]bool
+    // Eventuelt flere kanaler som:
+    MessageTxChannel         chan<- WorldView
+    MessageRxChannel         <-chan WorldView
+    OrderChannel             <-chan elevio.ButtonEvent
+    CompleteOrderChannel     <-chan fsm.DoneOrder
+    ConfirmedOrderChannel    chan WorldView
+    MergeFSMChannel          chan<- elevio.ButtonEvent
+}
